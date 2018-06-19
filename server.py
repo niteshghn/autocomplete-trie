4fa25cd0a7e93c3e9444b5f8ac23fa58251abea0
Nitesh Verma <niteshghn@gmail.com>
2018-06-19 16:24:56 +0530

    implemented scoring

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 
 from connector import Connector
-
+from operator import itemgetter
 app = Flask(__name__)
 
 
@@ -10,7 +10,7 @@ def hello_world():
     query = request.args.get("q", '')
     results = []  # must be list of dicts: [{"name": "foo"}, {"name": "bar"}]
     matches = Connector.search(query.lower())
-    results = [{'name': match, 'score': (len(query)/len(match))*10} for match in matches]
+    results = sorted([{'name': match, 'score': (len(query)/len(match))*10} for match in matches], key=itemgetter('score'), reverse=True)
     resp = jsonify(results=results[:10])  # top 10 results
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
